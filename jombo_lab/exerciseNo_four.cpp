1d1da21f53507a55bfb7f7964f518c446f1412bd
jombo-william <168138622+jombo-william@users.noreply.github.com>
2024-05-07 11:49:08 -0700

    Update exerciseNo_four.cpp

--- jombo_lab/exerciseNo_four.cpp
+++ jombo_lab/exerciseNo_four.cpp
@@ -1,3 +1,5 @@
+//entering user input number between 5 and 10
+
 #include <iostream>
 #include <limits> 
 #include <ios>    
@@ -7,17 +9,17 @@ using namespace std;
 int main() {
     int userInput_number;
 
-    // Prompt the user to enter  an integer value between 5 and 10
+    
     cout << "Please enter an integer value between 5 and 10: ";
     
-    // Keep prompting until a valid integer between 5 and 10 is entered
+    
     while (!(cin >> userInput_number) || userInput_number< 5 || userInput_number > 10) {
         cout << "Invalid input. Please enter an integer value between 5 and 10: ";
         cin.clear(); 
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
     }
 
-    // Output a message to inform the user that their input value has been accepted
+
     cout << "Your input value " << userInput_number << " has been accepted." << endl;
 
     return 0;
